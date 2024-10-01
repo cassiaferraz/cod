9b096d1e527ff026b7887807d3d09901178a31e9
Cassia Ferraz <153029234+cassiaferraz@users.noreply.github.com>
2024-10-01 10:49:35 -0300

    Update BoxPerfil.jsx

--- BoxPerfil/BoxPerfil.jsx
+++ BoxPerfil/BoxPerfil.jsx
@@ -1,7 +1,7 @@
 import usuario from '/img/svgs/avatarmasculino.png';
 import BarraProgresso from '../Progresso/BarraProgresso';
 import coin from '/img/svgs/Dolar_Dinero_Moneda_1Light.svg';
-import notificacao from '../../../../public/img/svgs/notificacao.svg'
+import notificacao from '../../../../public/img/svgs/notificacao.svg';
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import '../BoxPerfil/boxperfil.css';
@@ -11,7 +11,9 @@ function BoxPerfil({ serverIP, avatar }) {
   const [xp, setXp] = useState('');
   const [moedas, setMoedas] = useState('');
   const [userName, setUsername] = useState('');
-  const [currentAvatar, setCurrentAvatar] = useState(usuario); // Avatar padrão
+  const [currentAvatar, setCurrentAvatar] = useState(usuario);
+  const [notificacoes, setNotificacoes] = useState([]);
+  const [quantidadeNotificacoes, setQuantidadeNotificacoes] = useState(0);
 
   const token = sessionStorage.getItem('token');
 
@@ -27,13 +29,9 @@ function BoxPerfil({ serverIP, avatar }) {
         });
         const data = await response.json();
         setUsername(data.NOME);
-        sessionStorage.setItem('username', data.NOME);
         setMoedas(data.MOEDAS);
-        sessionStorage.setItem('usermoedas', data.MOEDAS);
         setNivel(data.NIVEL);
-        sessionStorage.setItem('usernivel', data.NIVEL);
         setXp(data.XP);
-        sessionStorage.setItem('userxp', data.XP);
       } catch (error) {
         console.log('Erro ao buscar os dados:', error);
       }
@@ -43,19 +41,57 @@ function BoxPerfil({ serverIP, avatar }) {
   }, [serverIP]);
 
   useEffect(() => {
-    // Atualiza o avatar com base no que vem do props
     if (avatar) {
       setCurrentAvatar(avatar);
     } else {
-      setCurrentAvatar(usuario); 
+      setCurrentAvatar(usuario);
     }
   }, [avatar]);
 
+  // Novo useEffect para buscar notificações
+  useEffect(() => {
+    async function fetchNotifications() {
+      try {
+        const response = await fetch(`${serverIP}/getUserNotifications`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'x-access-token': token
+          }
+        });
+        const notifications = await response.json();
+        setNotificacoes(notifications);
+        setQuantidadeNotificacoes(notifications.length);
+      } catch (error) {
+        console.log('Erro ao buscar notificações:', error);
+      }
+    }
+
+    fetchNotifications();
+  }, [serverIP, token]);
+
+  // Função para marcar notificações como lidas
+  const marcarComoLidas = async () => {
+    for (const notif of notificacoes) {
+      if (!notif.STATUS_LEITURA) {
+        await fetch(`${serverIP}/readNotification/${notif.ID_NOTIFICACAO}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+            'x-access-token': token
+          }
+        });
+      }
+    }
+    setQuantidadeNotificacoes(0); // Reseta o contador de notificações
+  };
+
   return (
     <div>
-      <Link to="/Perfil" style={{ textDecoration: 'none' }}>
+      <Link to="/Perfil" style={{ textDecoration: 'none' }} onClick={marcarComoLidas}>
         <header className="header-perfil">
-          <img id= "notificacao" src={notificacao} alt="not" />
+          {quantidadeNotificacoes > 0 && <span className="badge">{quantidadeNotificacoes}</span>}
+          <img id="notificacao" className="notificacao" src={notificacao} alt="not" />
           <img className="icon-usuario" src={currentAvatar} alt="usuario" />
           <div className="info">
             <div className="nome-e-nivel">
@@ -70,7 +106,6 @@ function BoxPerfil({ serverIP, avatar }) {
               <img className='coin' src={coin} alt="Ícone de Moedas" />
               {moedas}
             </div>
-              
           </div>
         </header>
       </Link>
