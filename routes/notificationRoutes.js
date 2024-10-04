5c127681b7c7b1aa66932b2540d45f6054835a15
Cassia Ferraz <153029234+cassiaferraz@users.noreply.github.com>
2024-10-04 10:58:04 -0300

    Update notificationRoutes.js

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -6,8 +6,8 @@ const router = express.Router();
 const notificationController = require('../controller/notificationController');
 
 router.post('/createNotification', notificationController.createNotification)
-// router.get('/getUserNotifications', verifyJwt, notificationController.getNotificationsByReceiverId)
-// router.put('/readNotification/:id', verifyJwt, notificationController.changeNotificationToReaded)
-// router.delete('/deleteNotification/:id', verifyJwt, notificationController.deleteNotification)
+router.get('/getUserNotifications', verifyJwt, notificationController.getNotificationsByReceiverId)
+router.put('/readNotification/:id', verifyJwt, notificationController.changeNotificationToReaded)
+router.delete('/deleteNotification/:id', verifyJwt, notificationController.deleteNotification)
 
 module.exports = router;
