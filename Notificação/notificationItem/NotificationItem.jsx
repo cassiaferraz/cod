c38591dc3adcf8e73004c03ab9691a4fc8f78b59
Cassia Ferraz <153029234+cassiaferraz@users.noreply.github.com>
2024-10-09 12:45:36 -0300

    Update NotificationItem.jsx

--- Notificação/notificationItem/NotificationItem.jsx
+++ Notificação/notificationItem/NotificationItem.jsx
@@ -1,38 +1,43 @@
-import fetchReadNotification from "../../../services/notifications/fetchReadNotification";
+import fetchReadNotification from "../../../services/notifications/fetchReadNotification"
 import { FaTrash } from "react-icons/fa6";
 import fetchDeleteNotification from "../../../services/notifications/fetchDeleteNotification";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-export default function NotificationItem({ serverIP, notification, handleRead, handleDelete }) {
-  const navigate = useNavigate();
-  const [isRead, setIsRead] = useState(notification?.STATUS_LEITURA);
-  const token = sessionStorage.getItem('token');
+export default function NotificationItem({ serverIP, notification, handleExclusion }) {
+    const navigate = useNavigate();
+    const [isRead, setIsRead] = useState(notification?.STATUS_LEITURA)  
 
-  const handleOnClick = async () => {
-    if (!notification?.STATUS_LEITURA) {
-      await handleRead(); // Chama a função de leitura do pai
-      setIsRead(true); // Marca como lida no estado
+    const handleOnClick = async (e) => {
+        if(!notification?.STATUS_LEITURA) {
+            const token = sessionStorage.getItem('token')
+            const response = await fetchReadNotification(token, notification?.ID_NOTIFICACAO)
+            if(!response.ok) alert("Erro ao ler notificação")
+            else setIsRead(true)
+        }
+        if(notification.REFERENCIA.path) navigate('/' + notification.REFERENCIA.path)
     }
 
-    if (notification.REFERENCIA?.path) {
-      navigate('/' + notification.REFERENCIA.path);
+    const handleExcludeNotification = async () => {
+        const token = sessionStorage.getItem('token')
+        console.log('EXCLUINDO NOTI')
+        const response = await fetchDeleteNotification(token, notification?.ID_NOTIFICACAO)
+        if(! response.ok) {
+            alert('Erro ao excluir notificação')
+        }
+        else handleExclusion(notification?.ID_NOTIFICACAO)
     }
-  };
 
-  const handleExcludeNotification = async () => {
-    console.log('Excluindo notificação...');
-    await handleDelete(); // Chama a função de exclusão do pai
-  };
-
-  return (
-    <div className="notification_item" style={isRead ? { color: 'var(--vivo-lightpurple50)' } : { color: 'var(--vivo-green)', fontWeight: '600' }}>
-      <div className="text" onClick={handleOnClick}>
-        {notification?.TEXTO}
-      </div>
-      <div className="actions">
-        <FaTrash className="trash" onClick={handleExcludeNotification} /> {/* Excluir apenas no clique */}
-      </div>
-    </div>
-  );
+    return (
+        <div className="notification_item" style={ isRead ? {color: 'var(--vivo-lightpurple50)'} : {color: 'var(--vivo-green)', fontWeight: '600'} }>
+            <div className="text" onClick={handleOnClick}>
+                { notification?.TEXTO }
+            </div>
+            <div className="actions">
+                {/* <GoUnread className="unread"/>
+                <GoRead className="read" /> */}
+                <FaTrash className="trash" onClick={() => handleExcludeNotification()} />
+            </div>
+        </div>
+    )
 }
