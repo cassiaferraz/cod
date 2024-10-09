57b526885a69134ac42044d099418849a8896545
Cassia Ferraz <153029234+cassiaferraz@users.noreply.github.com>
2024-10-09 12:39:11 -0300

    Update Notifications.jsx

--- Notificação/Notifications.jsx
+++ Notificação/Notifications.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState, useRef } from 'react';
 import './notification.css';
 import notification_foto from '../../../public/img/svgs/notificacao.svg';
 import NotificationItem from './notificationItem/NotificationItem';
-import fetchUserNotifications from '../../services/notifications/fetchUserNotifications';
 import fetchDeleteNotification from '../../services/notifications/fetchDeleteNotification';
 import fetchReadNotification from '../../services/notifications/fetchReadNotification';
 import React from 'react';
@@ -11,7 +10,7 @@ export default function Notifications({ notification, serverIP }) {
   const divRef = useRef(null);
   const token = sessionStorage.getItem('token');
   const [isMenuOpened, setIsMenuOpened] = useState(false);
-  const [notifications, setNotifications] = useState(notification);
+  const [notifications, setNotifications] = useState(notification); // Inicializa o estado com a prop
 
   const handleClickOutside = (e) => {
     if (divRef.current && !divRef.current.contains(e.target)) {
@@ -26,11 +25,16 @@ export default function Notifications({ notification, serverIP }) {
     };
   }, []);
 
+  // Use efeito para atualizar notifications quando a prop notification muda
+  useEffect(() => {
+    setNotifications(notification);
+  }, [notification]); // Dependência na prop notification
+
   const handleReadNotification = async (notificationId) => {
     const response = await fetchReadNotification({ token, notificationId, serverIP });
     if (response && response.ok) {
       console.log(`Notificação ${notificationId} marcada como lida.`);
-      removeNotificationFromList(notificationId); // Atualiza o estado
+      removeNotificationFromList(notificationId);
     } else {
       console.error('Erro ao marcar notificação como lida.');
     }
@@ -40,36 +44,36 @@ export default function Notifications({ notification, serverIP }) {
     const response = await fetchDeleteNotification({ token, notificationId, serverIP });
     if (response && response.ok) {
       console.log(`Notificação ${notificationId} excluída.`);
-      removeNotificationFromList(notificationId); // Remove do estado
+      removeNotificationFromList(notificationId);
     } else {
       console.error('Erro ao excluir a notificação.');
     }
   };
 
   const removeNotificationFromList = (notificationId) => {
-    let aux = [...notifications];
-    aux = aux.filter((not) => not.ID_NOTIFICACAO !== notificationId);
-    setNotifications(aux);
+    setNotifications((prevNotifications) =>
+      prevNotifications.filter((not) => not.ID_NOTIFICACAO !== notificationId)
+    );
   };
 
   return (
     <div className='notifications_menu' ref={divRef}>
       <div className='notification_icon' onClick={() => setIsMenuOpened((prev) => !prev)}>
         <img src={notification_foto} />
-        {notifications?.length > 0 ? <div className='notification_number'>{notifications.length}</div> : ''}
+        {notifications.length > 0 && <div className='notification_number'>{notifications.length}</div>}
       </div>
       <div className='notifications_dropdown' style={isMenuOpened ? {} : { display: 'none' }}>
         <div className='arrow'></div>
-        {notifications?.length > 0 ? (
+        {notifications.length > 0 ? (
           notifications.map((item, index) => (
             <React.Fragment key={`notification_${item?.TEXTO}_${index}`}>
               <NotificationItem
                 notification={item}
-                handleRead={() => handleReadNotification(item.ID_NOTIFICACAO)} // Passa a função de leitura
-                handleDelete={() => handleDeleteNotification(item.ID_NOTIFICACAO)} // Passa a função de exclusão
-                serverIP={serverIP} // Passa o serverIP para o componente filho
+                handleRead={() => handleReadNotification(item.ID_NOTIFICACAO)} 
+                handleDelete={() => handleDeleteNotification(item.ID_NOTIFICACAO)} 
+                serverIP={serverIP}
               />
-              {index !== notifications.length - 1 ? <div className='division'></div> : ''}
+              {index !== notifications.length - 1 && <div className='division'></div>}
             </React.Fragment>
           ))
         ) : (
