953f79ae081b6397ed2fe3283ec7348fecb67e5f
Cassia Ferraz <153029234+cassiaferraz@users.noreply.github.com>
2024-10-17 10:51:06 -0300

    Update NotificationItem.jsx

--- Notificação/notificationItem/NotificationItem.jsx
+++ Notificação/notificationItem/NotificationItem.jsx
@@ -4,7 +4,7 @@ import fetchDeleteNotification from "../../../services/notifications/fetchDelete
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-export default function NotificationItem({ serverIP, notification, handleExclusion }) {
+export default function NotificationItem({ notification, handleExclusion }) {
     const navigate = useNavigate();
     const [isRead, setIsRead] = useState(notification?.STATUS_LEITURA)  
 
@@ -18,18 +18,15 @@ export default function NotificationItem({ serverIP, notification, handleExclusi
         if(notification.REFERENCIA.path) navigate('/' + notification.REFERENCIA.path)
     }
 
-    const handleExcludeNotification = async () => { 
-        const token = sessionStorage.getItem('token'); 
-        const response = await fetchDeleteNotification({ 
-            token, 
-            notificationId: notification?.ID_NOTIFICACAO, serverIP });
-
-    if (!response.ok) {
-        alert('Erro ao excluir notificação');
-    } else {
-        handleExclusion(notification?.ID_NOTIFICACAO); // Chama a função passada como prop
+    const handleExcludeNotification = async () => {
+        const token = sessionStorage.getItem('token')
+        //console.log('HERE')
+        const response = await fetchDeleteNotification(token, notification?.ID_NOTIFICACAO)
+        if(! response.ok) {
+            alert('Erro ao excluir notificação')
+        }
+        else handleExclusion(notification?.ID_NOTIFICACAO)
     }
-    };
 
     return (
         <div className="notification_item" style={ isRead ? {color: 'var(--vivo-lightpurple50)'} : {color: 'var(--vivo-green)', fontWeight: '600'} }>
